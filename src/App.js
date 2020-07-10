051b619f7edb417c0d4ed2bcc798bcd595353537
Surendra Galwa <surendra.galwa@nymo.ai>
2020-07-10 15:02:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import scrollSnapPolyfill from 'css-scroll-snap-polyfill';
+
+function checkScroll() {
+	let videos = document.getElementsByTagName("video");
+
+	for (let i = 0; i < videos.length; i++) {
+
+		let video = videos[i];
+
+		var rect = video.getBoundingClientRect();
+
+
+		const visible = (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight) &&
+			rect.right <= (window.innerWidth)
+		);
+
+		if (visible) {
+			try {
+				video.play();
+			} catch (err) {
+				// do something
+			}
+
+		} else {
+			video.pause();
+		}
+	}
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	scrollSnapPolyfill();
+
+
+	useEffect(() => {
+		// on mount
+		if (window) {
+			checkScroll();
+			window.addEventListener('resize', checkScroll, false);
+		}
+		return () => {
+			window.removeEventListener('resize', checkScroll, false);
+		}
+	}, [])
+
+	return (
+		<div class="wrapper">
+			<div class="container" onScroll={checkScroll}>
+				<video src='/videos/v1.mov'
+					// autoPlay
+					controls
+					loop
+					muted
+				/>
+				<video src='/videos/v3.mp4'
+					controls
+					loop
+					muted
+				/>
+				<video src='/videos/v4.mp4'
+					controls
+					loop
+					muted
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
